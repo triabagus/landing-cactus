184447ac02b1882a8a51e337ff29b54e36dc4666
triabagus <triatop9@gmail.com>
2024-03-16 19:43:56 +0700

    [Scrollup] feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,13 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
 
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
